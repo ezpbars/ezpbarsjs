8390e262facd6df8d36b400a8debe0d095902a28
Amanda Moore <amanda.joy24@outlook.com>
2022-11-04 13:28:31 -0700

    basic test to test ci

---
+++ src/sum.test.ts
@@ -0,0 +1,11 @@
+import { sum } from './sum';
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+test('adds 1 + 2 to equal 4 (fails)', () => {
+  expect(sum(1, 2)).toBe(4);
+});
+
+export {};
